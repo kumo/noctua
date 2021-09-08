75600b25675addc0f430b11af9a0e93de65a7acd
Robert Clarke <kumo@kumo.it>
2021-09-08 22:51:10 +0200

    Cleaning up error tags

--- lib/noctua_web/views/error_helpers.ex
+++ lib/noctua_web/views/error_helpers.ex
@@ -10,8 +10,8 @@ defmodule NoctuaWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+      content_tag(:p, translate_error(error),
+        class: "mt-2 text-sm text-red-600",
         phx_feedback_for: input_name(form, field)
       )
     end)
