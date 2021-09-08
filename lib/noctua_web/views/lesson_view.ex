4539656814768bce5be4c4abd637ea0ea3f823f1
Robert Clarke <kumo@kumo.it>
2021-09-08 13:28:15 +0200

    Starting work on forms (requires new modules)

--- lib/noctua_web/views/lesson_view.ex
+++ lib/noctua_web/views/lesson_view.ex
@@ -3,17 +3,17 @@ defmodule NoctuaWeb.LessonView do
 
   def student_select(f, changeset) do
     student_opts =
-      for student <- Noctua.Enroling.list_students(),
-          do: [key: student.last_name, value: student.id]
+      for student <- Noctua.Enroling.list_alphabetical_students(),
+          do: [key: student.first_name <> " " <> student.last_name, value: student.id]
 
-    select(f, :student_id, student_opts, prompt: "Choose the student")
+    select(f, :student_id, student_opts, prompt: "", class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm")
   end
 
   def teacher_select(f, changeset) do
     teacher_opts =
-      for teacher <- Noctua.Teaching.list_teachers(),
-          do: [key: teacher.last_name, value: teacher.id]
+      for teacher <- Noctua.Teaching.list_alphabetical_teachers(),
+          do: [key: teacher.first_name <> " " <> teacher.last_name, value: teacher.id]
 
-    select(f, :teacher_id, teacher_opts, prompt: "Choose the teacher")
+    select(f, :teacher_id, teacher_opts, prompt: "", class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm")
   end
 end
