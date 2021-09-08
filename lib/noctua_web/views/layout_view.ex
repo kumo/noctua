e769018723188133d25ec5992b638ccada775810
Robert Clarke <kumo@kumo.it>
2021-09-08 15:43:08 +0200

    Highlighting the selected tab

--- lib/noctua_web/views/layout_view.ex
+++ lib/noctua_web/views/layout_view.ex
@@ -4,4 +4,13 @@ defmodule NoctuaWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def active_class(conn, path) do
+  current_path = Path.join(["/" | conn.path_info])
+  if path == current_path do
+    "border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+  else
+    "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+  end
+end
 end
