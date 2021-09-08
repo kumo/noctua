4d2f22bff2eb42daba9fc403fcc3056c6c07a27b
Robert Clarke <kumo@kumo.it>
2021-09-08 19:27:57 +0200

    Using simplified date and time selects

--- lib/noctua/timetabling/lesson.ex
+++ lib/noctua/timetabling/lesson.ex
@@ -7,6 +7,8 @@ defmodule Noctua.Timetabling.Lesson do
     field :ended_at, :naive_datetime # I wonder if when is more suitable? I could just do time for start and end
     field :note, :string
     field :started_at, :naive_datetime
+    field :date, :string, virtual: true
+    field :time, :string, virtual: true
     belongs_to :teacher, Noctua.Teaching.Teacher
     belongs_to :student, Noctua.Enroling.Student
 
@@ -16,11 +18,32 @@ defmodule Noctua.Timetabling.Lesson do
   @doc false
   def changeset(lesson, attrs) do
     lesson
-    |> cast(attrs, [:started_at, :ended_at, :note, :student_id, :teacher_id])
+    |> cast(attrs, [:started_at, :ended_at, :note, :student_id, :teacher_id, :date, :time])
+    |> timey_wimey()
     |> validate_required([:started_at, :ended_at, :student_id, :teacher_id])
   end
 
   def ordered(query) do
     from c in query, order_by: c.started_at
+  end
+
+  defp timey_wimey(changeset) do
+    case changeset do
+      %Ecto.Changeset{
+        valid?: true,
+        changes: %{date: date, time: time}
+      } ->
+        case Timex.parse("#{date} #{time}", "{D}/{M}/{YYYY} {h24}:{m}") do
+          {:ok, date} ->
+            changeset
+            |> put_change(:started_at, date)
+            |> put_change(:ended_at, Timex.shift(date, hours: 1))
+
+          {:error, _} ->
+            add_error(changeset, :date, "Invalid Date")
+        end
+      _ ->
+        changeset
+    end
   end
 end
--- lib/noctua_web/views/lesson_view.ex
+++ lib/noctua_web/views/lesson_view.ex
@@ -16,4 +16,29 @@ defmodule NoctuaWeb.LessonView do
 
     select(f, :teacher_id, teacher_opts, prompt: "", class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm")
   end
+
+  def simple_time_select(f, _changeset) do
+    # Probably should be able to select no time, but that messes up the changeset code
+    select(f, :time, ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"], class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm")
+  end
+
+  def simple_date_select(f, _changeset) do
+    today = Timex.now() |> Timex.beginning_of_day()
+    today_str = Timex.format!(today, "{0D}/{0M}/{YYYY}")
+
+    yesterday = Timex.shift(today, days: -1)
+    yesterday_str = Timex.format!(yesterday, "{0D}/{0M}/{YYYY}")
+
+    two_days_ago = Timex.shift(yesterday, days: -1)
+    two_days_ago_str = Timex.format!(two_days_ago, "{0D}/{0M}/{YYYY}")
+
+    three_days_ago = Timex.shift(two_days_ago, days: -1)
+    three_days_ago_str = Timex.format!(three_days_ago, "{0D}/{0M}/{YYYY}")
+
+    four_days_ago = Timex.shift(three_days_ago, days: -1)
+    four_days_ago_str = Timex.format!(four_days_ago, "{0D}/{0M}/{YYYY}")
+
+    select(f, :date, ["Oggi (#{today_str})": today_str, "Ieri (#{yesterday_str})": yesterday_str, "#{two_days_ago_str}": two_days_ago_str, "#{three_days_ago_str}": three_days_ago_str], class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm")
+  end
+
 end
