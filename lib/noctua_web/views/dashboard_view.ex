63431557c604967f4838b3f40f196fb7dfa27c7f
Robert Clarke <kumo@kumo.it>
2021-09-10 10:15:40 +0200

    Linking and correctly counting boxes on dashboard

--- lib/noctua_web/views/dashboard_view.ex
+++ lib/noctua_web/views/dashboard_view.ex
@@ -4,4 +4,16 @@ defmodule NoctuaWeb.DashboardView do
   def title("index.html", _assigns) do
     "Dashboard"
   end
+
+  def format_lesson(0) do
+    "Nessuna lezione"
+  end
+
+  def format_lesson(1) do
+    "1 lezione"
+  end
+
+  def format_lesson(n) do
+    "#{n} lezioni"
+  end
 end
