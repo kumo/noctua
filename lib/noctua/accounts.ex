0af95575d81f0f88dfe29da9900a8c5b466aaae7
Robert Clarke <kumo@kumo.it>
2021-12-13 11:09:02 +0100

    change_user needed

--- lib/noctua/accounts.ex
+++ lib/noctua/accounts.ex
@@ -362,4 +362,17 @@ defmodule Noctua.Accounts do
     |> Repo.all()
     |> Repo.preload(:teacher)
   end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user changes.
+
+  ## Examples
+
+      iex> change_user(user)
+      %Ecto.Changeset{data: %User{}}
+
+  """
+  def change_user(%User{} = user, attrs \\ %{}) do
+    User.changeset(user, attrs)
+  end
 end
