4ef2285571ca0875c74d8730a663611fd8c8db22
Robert Clarke <kumo@kumo.it>
2023-05-12 21:40:56 +0200

    Updating dates

--- test/support/fixtures/timetabling_fixtures.ex
+++ test/support/fixtures/timetabling_fixtures.ex
@@ -4,16 +4,24 @@ defmodule Noctua.TimetablingFixtures do
   entities via the `Noctua.Timetabling` context.
   """
 
+  import Noctua.TeachingFixtures
+  import Noctua.EnrolingFixtures
+
   @doc """
   Generate a lesson.
   """
   def lesson_fixture(attrs \\ %{}) do
+    student = student_fixture()
+    teacher = teacher_fixture()
+
     {:ok, lesson} =
       attrs
       |> Enum.into(%{
         ended_at: ~N[2021-09-06 12:37:00],
         note: "some note",
-        started_at: ~N[2021-09-06 12:37:00]
+        started_at: ~N[2021-09-06 12:37:00],
+        student_id: student.id,
+        teacher_id: teacher.id
       })
       |> Noctua.Timetabling.create_lesson()
 
