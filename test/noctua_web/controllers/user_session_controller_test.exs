7dd8ac982872c21673817f36d17edfd913190cbf
Robert Clarke <kumo@kumo.it>
2023-09-11 19:11:48 +0200

    Fixing login controller test

--- test/noctua_web/controllers/user_session_controller_test.exs
+++ test/noctua_web/controllers/user_session_controller_test.exs
@@ -11,9 +11,8 @@ defmodule NoctuaWeb.UserSessionControllerTest do
     test "renders log in page", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
-      assert response =~ "Register</a>"
-      assert response =~ "Forgot your password?</a>"
+      assert response =~ "Log in"
+      assert response =~ "Forgot your password?"
     end
 
     test "redirects if already logged in", %{conn: conn, user: user} do
