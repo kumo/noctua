1f25795b4a7df3ec14fdfad35518b52570f93cf9
Robert Clarke <kumo@kumo.it>
2023-09-12 18:42:56 +0200

    Removing missing CRUD from the user controller

--- lib/noctua_web/controllers/user_controller.ex
+++ lib/noctua_web/controllers/user_controller.ex
@@ -1,64 +1,8 @@
 defmodule NoctuaWeb.UserController do
   use NoctuaWeb, :controller
 
-  alias Noctua.Accounts
-  alias Noctua.Accounts.User
-
   def index(conn, _params) do
     users = Noctua.Accounts.list_users()
     render(conn, "index.html", users: users)
-  end
-
-  def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "User created successfully.")
-        |> redirect(to: Routes.user_path(conn, :show, user))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    lessons = Noctua.Timetabling.list_this_month_lessons(user)
-    students = Noctua.Reporting.list_this_months_student_stats_for_user(user)
-    render(conn, "show.html", user: user, lessons: lessons, students: students)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    changeset = Accounts.change_user(user)
-    render(conn, "edit.html", user: user, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
-
-    case Accounts.update_user(user, user_params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "User updated successfully.")
-        |> redirect(to: Routes.user_path(conn, :show, user))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", user: user, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    {:ok, _user} = Accounts.delete_user(user)
-
-    conn
-    |> put_flash(:info, "User deleted successfully.")
-    |> redirect(to: Routes.user_path(conn, :index))
   end
 end
