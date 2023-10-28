46b26a9bb11155b8d6efcce39410c3954f81648d
Robert Clarke <kumo@kumo.it>
2023-10-28 17:42:32 +0200

    Showing student checkboxes in order and in more spce

--- lib/noctua_web/components/core_components.ex
+++ lib/noctua_web/components/core_components.ex
@@ -365,7 +365,7 @@ defmodule NoctuaWeb.CoreComponents do
     # IO.inspect assigns
 
     ~H"""
-    <div phx-feedback-for={@name} class="text-sm">
+    <div phx-feedback-for={@name} class="text-sm col-span-6">
       <.label for={@id}><%= @label %></.label>
       <div class="mt-1 w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         <div class="grid grid-cols-1 gap-1 text-sm items-baseline">
