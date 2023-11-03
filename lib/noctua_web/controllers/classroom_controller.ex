4e6d8c42f77582884aee7bba775526ebec26e15f
Robert Clarke <kumo@kumo.it>
2023-11-03 14:53:05 +0100

    Filtering classrooms per current user

--- lib/noctua_web/controllers/classroom_controller.ex
+++ lib/noctua_web/controllers/classroom_controller.ex
@@ -7,7 +7,12 @@ defmodule NoctuaWeb.ClassroomController do
   require Logger
 
   def index(conn, _params) do
-    classrooms = Timetabling.list_ordered_classrooms()
+    classrooms = if conn.assigns.current_user.role == :Teacher do
+      Timetabling.list_ordered_classrooms_for_teacher_id(conn.assigns.current_user.teacher.id)
+    else
+      Timetabling.list_ordered_classrooms()
+    end
+
     render(conn, "index.html", classrooms: classrooms)
   end
 
